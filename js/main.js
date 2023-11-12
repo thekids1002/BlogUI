5a680a9c45277a70c740bd0220db7859b71d5366
thekids1002 <phimtonghop7879@gmail.com>
2023-11-12 10:00:50 +0700

    done blog UI

--- js/main.js
+++ js/main.js
@@ -12,3 +12,47 @@ setInterval(updateText, 2000);
 
 // Gọi hàm cập nhật nội dung lần đầu
 updateText();
+
+
+// Lắng nghe sự kiện cuộn trang
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    
+    // Hiển thị nút khi người dùng cuộn xuống dưới
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// Xử lý sự kiện khi nút "Nổi lên đầu trang" được nhấn
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+    scrollToTop();
+});
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+
+// Lắng nghe sự kiện cuộn trang
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // Hiển thị nút khi người dùng cuộn xuống dưới
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
